c74bc5551e61f597baec45252ccd00683a7b0d63
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CrosshairLabelTemplateForAxis.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrosshairLabelTemplateForAxis.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrosshairLabelTemplateForAxis.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("304de32d-bd66-4bde-9c4f-f66b52a5a957")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to customize the appearance of crosshair axis labels
+
+
+<p>This example shows how to customize the visual representation of crosshair axis labels via their template. <br />
+</p><p>To accomplish this, it is necessary to create a <strong>System.Windows.DataTemplate</strong> object that specifies the appearance of crosshair axis labels, and assign it to the <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsAxis2D_CrosshairLabelTemplatetopic"><u>Axis2D.CrosshairLabelTemplate</u></a> property.</p><p><br />
+</p>
+
+<br/>
+
+
